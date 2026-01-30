bb313276c7e94d5026a9b6ea2b1b5eb62214035a
Ethan Huang <69299174+chingweih@users.noreply.github.com>
2026-01-31 02:20:42 +0800

    feat(api): send jobs to queue

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,9 +1,25 @@
 import { Hono } from 'hono'
+import { Queue } from 'bullmq'
+import { videoTranscodeQueue } from '@video-stream/shared/contract'
+import type {
+  VideoTranscodeQueueData,
+  VideoTranscodeJobNames,
+} from '@video-stream/shared/contract'
 
 const app = new Hono()
 
+const queue = new Queue<VideoTranscodeQueueData, void, VideoTranscodeJobNames>(
+  videoTranscodeQueue,
+  {
+    connection: {
+      url: process.env.REDIS_URL,
+    },
+  },
+)
+
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  queue.add('transcode', { videoId: 'hello' })
+  return c.text('Job Sent')
 })
 
 export default app
