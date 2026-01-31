75feae15e86334b4e3a5fea916c42b708a4502b5
Ethan Huang <69299174+chingweih@users.noreply.github.com>
2026-01-31 17:00:58 +0800

    feat(worker): use ffmpeg to process videos in .tmp folder

--- packages/worker/src/index.ts
+++ packages/worker/src/index.ts
@@ -2,6 +2,8 @@ import { Worker } from 'bullmq'
 import { videoTranscodeQueue } from '@video-stream/shared/contract'
 import type { VideoTranscodeQueueData } from '@video-stream/shared/contract'
 import type { VideoTranscodeJobNames } from '@video-stream/shared/contract'
+import { mkdir, readdir, rm } from 'node:fs/promises'
+import { s3 } from 'bun'
 
 const worker = new Worker<
   VideoTranscodeQueueData,
@@ -10,7 +12,44 @@ const worker = new Worker<
 >(
   videoTranscodeQueue,
   async ({ data: { videoId } }) => {
-    console.log(videoId)
+    const dir = `.tmp/videos/${videoId}`
+    await mkdir(`${dir}/output`, { recursive: true })
+
+    const originalVideo = s3.file(`/videos/original/${videoId}`)
+    await Bun.write(`${dir}/original`, await originalVideo.arrayBuffer())
+
+    const ffmpegProc = Bun.spawn({
+      cmd: [
+        'ffmpeg',
+        '-i',
+        'original',
+        '-hls_time',
+        '10',
+        '-hls_list_size',
+        '0',
+        '-f',
+        'hls',
+        '-c:v',
+        'h264_videotoolbox',
+        'output/master.m3u8',
+      ],
+      cwd: dir,
+    })
+
+    await ffmpegProc.exited
+
+    const processedFiles = await readdir(`${dir}/output`)
+
+    await Promise.all(
+      processedFiles.map((file) =>
+        s3.write(
+          `/videos/${videoId}/${file}`,
+          Bun.file(`${dir}/output/${file}`),
+        ),
+      ),
+    )
+
+    await rm(dir, { recursive: true })
   },
   {
     connection: {
